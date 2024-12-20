0acc8f2de9d749c5d92346e9daacb800b0e98061
Rohit Shelar <rohitpshelar@gmail.com>
2024-12-20 16:27:11 +0530

    resilience4j retry added

--- UserService/UserService/src/main/java/com/example/user/service/controllers/UserController.java
+++ UserService/UserService/src/main/java/com/example/user/service/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.user.service.controllers;
 import com.example.user.service.entities.User;
 import com.example.user.service.services.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,13 +29,21 @@ public class UserController {
     }
 
     @GetMapping("/{uid}")
-    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+//    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    @Retry(name = "ratingHotelRetry", fallbackMethod = "ratingHotelRetryFallback")
     public ResponseEntity<User> getUser(@PathVariable String uid){
+        logger.info("getUser is executed for Udrt Id : {}",uid);
         return ResponseEntity.ok(userService.getUser(uid));
     }
 
     public ResponseEntity<User> ratingHotelFallback(String uid, Exception ex){
-        logger.info("Fallback is executed because service is down : {}",ex.getMessage());
+        logger.info("Circuit Fallback is executed because service is down : {}",ex.getMessage());
+        var user = User.builder().email("dummy").name("dummy").about("Created dummy as some service is down").uid("123").build();
+        return ResponseEntity.ok(user);
+    }
+
+    public ResponseEntity<User> ratingHotelRetryFallback(String uid, Exception ex){
+        logger.info("Retry Fallback is executed because service is down : {}",ex.getMessage());
         var user = User.builder().email("dummy").name("dummy").about("Created dummy as some service is down").uid("123").build();
         return ResponseEntity.ok(user);
     }
